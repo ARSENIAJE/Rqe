eedb9f527e70f5c9923bb810ccfe86ce8c6bea17
ARSENIAJE <150345148+ARSENIAJE@users.noreply.github.com>
2026-05-11 14:38:41 +0300

    aeae
    Removed the code block that deletes the OpeningCrates folder.

--- ppd.lua
+++ ppd.lua
@@ -1,12 +1,3 @@
--- 1. Удаляем папку OpeningCrates целиком
-local openingCrates = workspace:FindFirstChild("OpeningCrates")
-if openingCrates then
-    openingCrates:Destroy()
-    print("✅ Папка OpeningCrates удалена")
-else
-    print("⚠️ Папка OpeningCrates не найдена")
-end
-
 -- 2. Удаляем ResultsGui у игрока
 local player = game.Players.LocalPlayer
 local playerGui = player:FindFirstChild("PlayerGui")
